bcff5ad27b4c051077ccf204d796abc8c5f988fc
George Alan Rufo <georgealanrufo@gmail.com>
2024-08-21 14:28:42 -0300

    Zone and Signal examples in user component

--- code-notes/02-essentials/01-starting-project/src/app/user/user.component.ts
+++ code-notes/02-essentials/01-starting-project/src/app/user/user.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component, computed, signal} from '@angular/core';
+import { DUMMY_USERS } from "../dummy-users";
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
@@ -7,4 +10,23 @@ import { Component } from '@angular/core';
   styleUrl: './user.component.css'
 })
 
-export class UserComponent {}
+export class UserComponent {
+  /*
+  All features here will be available in the templateUrl component.
+  If we add private to the properties here it only will be available here in this
+  component, but if it's without declaration then will be public and accessible in
+  templateUrl.
+  */
+  selectedUser = signal(DUMMY_USERS[randomIndex]);
+  imagePath = computed(() => 'users/' + this.selectedUser().avatar)
+
+  // This code below was replaced by the above, which use signal approach.
+  // get imagePath() {
+  //   return 'users/' + this.selectedUser.avatar
+  // }
+
+  onSelectUser() {
+    const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
+    this.selectedUser.set(DUMMY_USERS[randomIndex]);
+  }
+}
